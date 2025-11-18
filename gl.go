aa210559fdd960625de27a2e6dc32cb370b8ed1a
ayxank <67361423+ayxank@users.noreply.github.com>
2025-11-18 00:13:02 -0800

    Enhance GitHub to GitLab transfer functionality
    Added functionality to clone a GitHub repository, download a release zip file, and push to GitLab.

--- gl.go
+++ gl.go
@@ -2,42 +2,63 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing/object"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
 )
 
 func main() {
 
 	// Create inputs
-	var gh_URL, gh_Token, gl_URL, gl_Token, gl_Usr, gl_Ml string
+	var gh_URL, gh_Token, gh_rel, gl_URL, gl_Token, URL_releases, File_name string
 	fmt.Println("Write your Github URL(http): ")
 	fmt.Scanln(&gh_URL)
 	fmt.Println("Write your Github Token: ")
 	fmt.Scanln(&gh_Token)
+	fmt.Println("Write Github releas name ('repo name' + '-' + 'releas name'): ")
+	fmt.Scanln(&gh_rel)
 	fmt.Println("Write your Gitlab URL(http): ")
 	fmt.Scanln(&gl_URL)
 	fmt.Println("Write your Gitlab Token: ")
 	fmt.Scanln(&gl_Token)
-	fmt.Println("Write your Gitlab username: ")
-	fmt.Scanln(&gl_Usr)
-	fmt.Println("Write your Gitlab mail: ")
-	fmt.Scanln(&gl_Ml)
+
 	// Cloning from the Github
 
 	_, err := git.PlainClone("./repo", false, &git.CloneOptions{
 		URL: gh_URL,
-		Auth: &http.BasicAuth{
+		Auth: &githttp.BasicAuth{
 			Username: "empty",
 			Password: gh_Token,
 		},
 	})
 
+	if err != nil {
+		panic(err)
+	}
+	// Clonning from the Github (releases)
+	URL_releases = "https://github.com/ayxank/test/archive/refs/tags/assad.zip"
+	File_name = gh_rel + ".zip"
+
+	resp, err := http.Get(URL_releases)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	out, err := os.Create(File_name)
+	if err != nil {
+		panic(err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		panic(err)
 	}
@@ -71,9 +92,7 @@ func main() {
 		AllowEmptyCommits: true,
 
 		Author: &object.Signature{
-			Name:  gl_Usr,
-			Email: gl_Ml,
-			When:  time.Now(),
+			When: time.Now(),
 		},
 	})
 	if err != nil {
@@ -96,7 +115,7 @@ func main() {
 	err = repo.Push(&git.PushOptions{
 		RemoteURL: gl_URL,
 		Force:     true,
-		Auth: &http.BasicAuth{
+		Auth: &githttp.BasicAuth{
 			Username: "empty",
 			Password: gl_Token,
 		},
@@ -108,10 +127,18 @@ func main() {
 
 	fmt.Println("Successfully pushed.")
 
+	// Push to the remote (releases)
+
 	// Removing folders from the repository
 
 	err = os.RemoveAll("./repo")
 	if err != nil {
 		log.Fatalf("Error removing directory: %v", err)
 	}
+
+	err = os.RemoveAll("./" + File_name)
+	if err != nil {
+		log.Fatalf("Error removing directory: %v", err)
+	}
+
 }
